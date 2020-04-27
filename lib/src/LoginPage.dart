ea2bba39f3091b2bf849045a00396b3043a12914
lvshaojun <lvshaojun@cmhi.chinamobile.com>
2020-04-27 15:50:32 +0800

    OCT complete MainPage frame and jump function from LoginPage to MainPage.

--- lib/src/LoginPage.dart
+++ lib/src/LoginPage.dart
@@ -1,11 +1,21 @@
+import 'package:douban/src/MainPage.dart';
 import 'package:flutter/material.dart';
-
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LoginWidget(),
+    );
+  }
 }
 
-class _LoginPageState extends State<LoginPage> {
+
+class LoginWidget extends StatefulWidget {
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
   bool enableLogin = false;
@@ -33,85 +43,73 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.teal.shade300,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('images/icon_douban.jpg'),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Card(
-                  color: Colors.white,
-                  child: TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'username',
-                      icon: Icon(Icons.person),
-                    ),
-                    controller: usernameController,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Card(
-                  child: TextField(
-                    autofocus: true,
-                    decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: 'password',
-                      icon: Icon(Icons.lock),
-                    ),
-                    controller: passwordController,
-                    obscureText: true,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              FlatButton(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 15, horizontal: 80),
-                child: Text('Login',
-                  style: TextStyle(color: Colors.white, fontSize: 20),),
-                color: enableLogin ? Colors.blue : Colors.grey.shade400,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(40)),
-                ),
-                onPressed: _onLoginPressed,
-              ),
-            ],
+    return Scaffold(
+      backgroundColor: Colors.teal.shade300,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('images/icon_douban.jpg'),
           ),
-        ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Card(
+              color: Colors.white,
+              child: TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'username',
+                  icon: Icon(Icons.person),
+                ),
+                controller: usernameController,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Card(
+              child: TextField(
+                autofocus: true,
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: 'password',
+                  icon: Icon(Icons.lock),
+                ),
+                controller: passwordController,
+                obscureText: true,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          FlatButton(
+            padding: const EdgeInsets.symmetric(
+                vertical: 15, horizontal: 80),
+            child: Text('Login',
+              style: TextStyle(color: Colors.white, fontSize: 20),),
+            color: enableLogin ? Colors.blue : Colors.grey.shade400,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(40)),
+            ),
+            onPressed:_onLoginPress,
+          ),
+        ],
       ),
     );
   }
-
-  void _onLoginPressed() {
-    if (enableLogin) {
-
-    }
-    @override
-    void dispose() {
-      usernameController.dispose();
-      passwordController.dispose();
-      super.dispose();
-    }
+  void _onLoginPress(){
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>MainPage()),);
   }
+
 }
 
 
---
+++ lib/src/MainPage.dart
@@ -0,0 +1,66 @@
+
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'MePage.dart';
+import 'TopPage.dart';
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MainWidget(),);
+  }
+}
+
+class MainWidget extends StatefulWidget {
+  @override
+  _MainWidgetState createState() => _MainWidgetState();
+}
+
+class _MainWidgetState extends State<MainWidget> {
+  var _pages=List<Widget>();
+  var _titles=List<String>();
+  var _bottomBarItems=List<BottomNavigationBarItem>();
+  var _selectedIndex=0;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _pages
+      ..add(HomePage())
+      ..add(TopPage())
+      ..add(MePage());
+    _titles
+    ..add('Home')
+    ..add('Top')
+    ..add('Me');
+    _bottomBarItems
+    ..add(BottomNavigationBarItem(title: Text('Home'),icon: Icon(Icons.home),))
+    ..add(BottomNavigationBarItem(title: Text('Top'),icon: Icon(Icons.movie),))
+    ..add(BottomNavigationBarItem(title: Text('Me'),icon: Icon(Icons.person),));
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text(_titles[_selectedIndex]),
+        centerTitle: true,
+      ),
+      body:_pages[_selectedIndex] ,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        items:_bottomBarItems,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+  void _onItemTapped(int index){
+    setState(() {
+      _selectedIndex=index;
+    });
+  }
+
+}
+

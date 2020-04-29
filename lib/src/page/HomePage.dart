61f171495f661f006f2b6155053a2b0ab808fbd8
lvshaojun <lvshaojun@cmhi.chinamobile.com>
2020-04-29 16:40:53 +0800

    OCT finish movies in theater function of HomePage .

--- lib/src/page/HomePage.dart
+++ lib/src/page/HomePage.dart
@@ -1,8 +1,9 @@
+import 'dart:convert' as convert;
+
 import 'package:douban/src/model/movies.dart';
 import 'package:douban/src/model/subjects.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert' as convert;
 
 class HomePage extends StatelessWidget {
   @override
@@ -36,32 +37,131 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-              )
+              ),
             ],
           ),
-          ListView.builder(scrollDirection: Axis.horizontal,itemBuilder:(context,index){
-            return Column(
-              children: <Widget>[
-                Image.network('src',width: 60,height: 100,),
-                Text('name'),
-                Row(
-
-                )
-              ],
-            );
-          })
+          Container(
+            height: 200,
+            child: _MovieListView(),
+          ),
         ],
       ),
     );
   }
+}
 
-  Future<List<Subjects>> _getInTheaterMovies() async{
-   final response= await http.get('https://douban.uieee.com/v2/movie/in_theaters');
-   if(response.statusCode==200){
-     final movies=Movies.fromJson(convert.jsonDecode(response.body));
-     return movies.subjects;
-   }else{
-      return null;
-   }
+class _MovieListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<Subjects>>(
+      future: _getInTheaterMovies(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+         return  Text('加载失败');
+        }
+        return snapshot.hasData
+            ? ListView.builder(
+                itemCount: snapshot.data.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return _ListItem(snapshot.data[index]);
+                },
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              );
+      },
+    );
+  }
+
+  Future<List<Subjects>> _getInTheaterMovies() async {
+    final response =
+        await http.get('https://douban.uieee.com/v2/movie/in_theaters');
+    if (response.statusCode == 200) {
+      final movies = Movies.fromJson(convert.jsonDecode(response.body));
+      return movies.subjects;
+    } else {
+      throw Exception('failed to get movies');
+    }
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  final Subjects subject;
+
+  _ListItem(this.subject);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 120,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.network(
+            subject.images.small,
+            width: 100,
+            height: 150,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            subject.title,
+            style: TextStyle(fontWeight: FontWeight.bold),
+            overflow: TextOverflow.ellipsis,
+          ),
+          _RatingItem(
+            subject.rating.average,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _RatingItem extends StatelessWidget {
+  final double rating;
+  final starSize = 15.0;
+
+  _RatingItem(this.rating);
+
+  @override
+  Widget build(BuildContext context) {
+    int startNum;
+    if (rating <= 10 && rating > 8) {
+      startNum = 5;
+    } else if (rating <= 8 && rating > 6) {
+      startNum = 4;
+    } else if (rating <= 6 && rating > 4) {
+      startNum = 3;
+    } else if (rating <= 4 && rating > 2) {
+      startNum = 2;
+    } else {
+      startNum = 1;
+    }
+    List<Widget> starIcons = <Widget>[];
+    for (int i = 0; i < 5; i++) {
+      if (i < startNum) {
+        starIcons.add(Icon(
+          Icons.star,
+          color: Colors.yellow,
+          size: starSize,
+        ));
+      } else {
+        starIcons.add(Icon(
+          Icons.star,
+          color: Colors.grey.shade200,
+          size: starSize,
+        ));
+      }
+    }
+    starIcons.add(Text(
+      '$rating',
+      style: TextStyle(color: Colors.grey, fontSize: 12),
+    ));
+    return Row(
+      children: starIcons,
+    );
   }
 }
